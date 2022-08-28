9dc82ca0d1c49ffe723d064197a0cb3280e39221
Alexey Karev <per4illo@mail.ru>
2022-08-28 23:09:24 +0300

    первый комит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 0;
+        while ( i < 10) {
+            i++;
+            System.out.print(" "+i);
+        }
+        System.out.println();
+        while ( i > 0) {
+            System.out.print(" "+i);
+            i--;
+        }
+        System.out.println();
+        int friday = 1;
+        while ( friday < 31) {
+            System.out.println("Сегодня пятница, " + friday + "-е число.");
+            friday = friday +7;
+        }
+        System.out.println();
+
+        int day = 2022;
+        int j = 0;
+        int day100 = day + 100;
+        day = day - 200;
+        while ( j < day100 ) {
+            if (j > day && j < day100)
+            System.out.println(j);
+            j = j + 79;
+
+        }
+
     }
 }
