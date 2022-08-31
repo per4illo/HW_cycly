f4d99755334d72d8eec6f060b386ae9f0c25ca11
Alexey Karev <per4illo@mail.ru>
2022-08-31 19:25:54 +0300

    сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         int i = 0;
-        while ( i < 10) {
+        while (i < 10) {
             i++;
-            System.out.print(" "+i);
+            System.out.print(" " + i);
         }
         System.out.println();
-        while ( i > 0) {
-            System.out.print(" "+i);
+        while (i > 0) {
+            System.out.print(" " + i);
             i--;
         }
         System.out.println();
         int friday = 1;
-        while ( friday < 31) {
+        while (friday < 31) {
             System.out.println("Сегодня пятница, " + friday + "-е число.");
-            friday = friday +7;
+            friday = friday + 7;
         }
         System.out.println();
 
@@ -22,7 +22,7 @@ public class Main {
         int j = 0;
         int day100 = day + 100;
         day = day - 200;
-        while ( j < day100 ) {
+        while (j < day100) {
             if (j > day && j < day100) {
                 System.out.println(j);
                 j = j + 79;
